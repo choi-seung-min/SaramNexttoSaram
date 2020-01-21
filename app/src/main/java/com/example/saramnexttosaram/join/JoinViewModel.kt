11d5a66e8232a3a6a8afccb768b32417d63593bb
seung min <3235tmdals@gmail.com>
2020-01-21 19:10:20 +0900

    [Modify] changed activity login/join edit text attributes

--- app/src/main/java/com/example/saramnexttosaram/join/JoinViewModel.kt
+++ app/src/main/java/com/example/saramnexttosaram/join/JoinViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.saramnexttosaram.join
 
-import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
